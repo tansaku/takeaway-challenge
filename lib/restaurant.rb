670f04f44b541421fa91add83519ca1237a847b0
Sam Joseph <tansaku@gmail.com>
2015-03-30 06:55:16 +0100

    ready to send text if price is correct

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,11 +1,13 @@
+require_relative 'order'
+
 class Restaurant
   def dishes
     { chicken: '£1', fish: '£2' }
   end
 
-  def order hash
+  def order hash, expected_total
     order = Order.new self
     hash.each { |k, v| order.send k, v }
-    order
+    send_text if order.total == expected_total
   end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -5,12 +5,16 @@ describe Restaurant do
     expect(subject.dishes).to eq(chicken: '£1', fish: '£2')
   end
   it 'can create single item order' do
+    expect(subject).to receive :send_text
     expect_any_instance_of(Order).to receive(:chicken).with(3)
-    subject.order(chicken: 3)
+    expect_any_instance_of(Order).to receive(:total).and_return('£3')
+    subject.order({ chicken: 3 }, '£3')
   end
   it 'can create multi-item order' do
+    expect(subject).to receive :send_text
     expect_any_instance_of(Order).to receive(:chicken).with(3)
     expect_any_instance_of(Order).to receive(:fish).with(2)
-    subject.order(chicken: 3, fish: 2)
+    expect_any_instance_of(Order).to receive(:total).and_return('£7')
+    subject.order({ chicken: 3, fish: 2 }, '£7')
   end
 end
